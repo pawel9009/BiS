8aea2336ff35d05dba6d0d2bafbead7b9ff4e2f4
pawel9009 <pawel.b512222@gmail.com>
2023-01-10 00:25:26 +0100

    add validation

--- conections.py
+++ conections.py
@@ -1,7 +1,9 @@
 import db_info
 import get_data
+from pathlib import Path
 from db_info import *
 from sqlalchemy import create_engine
+from validation import validate_data
 
 
 def connect():
@@ -11,6 +13,14 @@ def connect():
         conn = db_config
 
         df = get_data.scrap_data()
+        df.to_csv(Path('files/data.csv'), index=False)
+        testowa = df.head(5)
+
+        # testowa.Zamkniecie.iloc[1] = 'asdsd'
+        # testowa.Otwarcie.iloc[3] = 'asdsd'
+        # testowa.Data.iloc[3] = 'asdsd'
+
+        df = validate_data(testowa)
 
         engine = create_engine(f"postgresql://{db_info.user}:{db_info.password}@localhost/{db_info.database}")
 
@@ -20,6 +30,7 @@ def connect():
             if_exists='replace',
             index=True,
         )
+
 
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
